36ad0c30a45e13db918e152a53d3b7c56f89b79d
letiesperon <esperonleticia@gmail.com>
2019-05-22 16:43:50 -0300

    Fix url

--- services/after/uses.rb
+++ services/after/uses.rb
@@ -17,7 +17,7 @@ final_message = FinalMessageBuilder.final_sms_message(engagmement.sms_message,
                                                      engagement.discount_code)
 
 # use n4
-replace_from = customer_url_shortener&.short_url
+replace_from = url_shortener&.short_url
 replate_to = customer_url_shortener&.short_url
 final_message = MessageUtils.replace_text(engagmement.sms_message, replace_from, replace_to)
 final_message = FinalMessageBuilder.final_sms_message(final_message,
